371c12b72f7ac93febfa6bbb47e0b25d34f0a625
LucasIsidoroGalusni <isidorogalusni@gmail.com>
2021-05-26 14:32:22 +0000

    aula01-fundamentos01

---
+++ pwirea/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Olamundo from './components/basico/Olamundo'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Fundamentos 01 - fundamentos01</h1>
+        <Olamundo />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ pwirea/src/components/basico/Olamundo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+
+    const informação = 'Primeiro Componente jsx'
+
+    return(
+        <>
+        <h2>Componente Olamundo</h2>
+        <p>{informação}</p>
+        </>
+    )
+}
